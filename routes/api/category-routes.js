5f5366274874592636f85303669f8808f20eca84
Leithen <leithen113@gmail.com>
2021-12-02 23:27:20 -0700

    Added update and delete routes for all models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,9 +44,7 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-  Category.create({
-    category_name: req.body.category_name
-  })
+  Category.create({ category_name: req.body.category_name })
   .then(dbCategoryData => res.json(dbCategoryData))
   .catch(err => {
     console.log(err)
@@ -56,12 +54,30 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-
+  Category.update({ category_name: req.body.category_name }, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -118,7 +118,16 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-  
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
