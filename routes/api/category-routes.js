e693148cf741a9435c9ed27cdfc5577f9ce2c6bd
Leithen <leithen113@gmail.com>
2021-12-02 22:40:50 -0700

    Created findAll routes for all models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,37 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [{
+      model: Product,
+    }]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  
 });
 
 router.post('/', (req, res) => {
   // create a new category
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,37 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{
+      model: Product,
+    }]
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+
 });
 
 module.exports = router;
